82ca00a12129a7a83263f6a9aa802967803bd200
Ariel Penaloza <Ariel Penaloza@STFU>
2026-04-24 20:22:36 -0700

    Creating Enemy Class With their getters

---
+++ src/game/Enemy.java
@@ -0,0 +1,24 @@
+package game;
+
+public class Enemy {
+	private String name, rank;
+	private double hp, attack;
+	private boolean isBoss;
+	
+	public Enemy(String name, String rank, double hp, double attack, boolean isBoss) {
+		this.name = name;
+		this.rank = rank;
+		this.hp = hp;
+		this.attack = attack;
+		this.isBoss = isBoss;
+	}
+	
+	public void takeDamage(double dmg) { this.hp = Math.max(0, this.hp - dmg); }
+	public boolean isAlive() { return hp > 0; }
+	
+	public String getName() { return name; }
+	public double getHp() { return hp; }
+	public double getAttack() { return attack; }
+	public String getRank() { return rank; }
+	public boolean isBoss() { return isBoss; }
+}
